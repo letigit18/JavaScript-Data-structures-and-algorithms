b2bdf6ba6defba342b3182a1c085d7b76b4e0ef1
Mersi <mersi.tesfaye18@gmail.com>
2023-12-03 01:23:42 -0800

    reduce() array function implemented

---
+++ reduce.js
@@ -0,0 +1,19 @@
+//reduce function is used for reducing the array elements into a single value eg. summing the array elements
+//reduce(accumulator, elementvalue, index, array)=>{}, initial value for the total
+const numbers = [3, 5, 5, 9]
+let sum = numbers.reduce((total, number)=>{
+    return total + number
+}, 0)
+console.log(sum)
+//reducer used on the array of objects
+const item = [
+    {name: "orange", price: 32},
+    {name: "banana", price: 44},
+    {name: "apple", price: 50},
+    {name: "Mango", price: 25}
+]
+const totalPrice = item.reduce((total, item)=>{
+    return total + item.price
+}, 0)
+console.log(`total price of the fruits is = ${totalPrice}`)
+console.log(item["name"])
